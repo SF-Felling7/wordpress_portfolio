f9472c6567d7ce7fcdfeece8f8ca53e66ba2f1a8
Sean Felling <Snfelling7@gmail.com>
2017-09-26 17:03:57 -0500

    added enqueueing scripts

--- wp-content/themes/portfolio_theme/functions.php
+++ wp-content/themes/portfolio_theme/functions.php
@@ -1,1 +1,9 @@
+<?php
+function my_own_styles() {
 
+
+    wp_enqueue_style( 'portfolio_theme', get_template_directory_uri() . '/css/portfolio.css' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'my_own_styles()' );
