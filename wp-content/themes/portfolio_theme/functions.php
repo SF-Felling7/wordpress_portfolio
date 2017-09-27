b81f04bb84ddc8f157ee8cbe615231651b1d8bdb
Sean Felling <Snfelling7@gmail.com>
2017-09-27 15:34:53 -0500

    changed function URI

--- wp-content/themes/portfolio_theme/functions.php
+++ wp-content/themes/portfolio_theme/functions.php
@@ -2,11 +2,12 @@
 
 function my_own_styles() {
 
-
-    wp_enqueue_style( 'portfolio_theme', get_template_directory_uri() . '/css/myCartera.css' );
+    wp_enqueue_style( 'portfolio_theme', get_stylesheet_directory_uri() .
+'/css/portfolio.css' );
 
 }
 
-add_action( 'wp_enqueue_scripts', 'my_own_styles');
+add_action( 'wp_enqueue_scripts', 'my_own_styles' );
+
 
  ?>
